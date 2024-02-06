9595af5035dd6ad46a9b91f49abb9679d0ecac19
Lucas Damaceno <ldamaceno@outlook.com>
2024-02-05 21:56:52 -0300

    implemented question one, improved module structure and refactored tests

---
+++ src/main.py
@@ -0,0 +1,15 @@
+class A():
+    def __init__(self, a1=1, a2=2, a3=3):
+        self.a1 = a1
+        self.a2 = a2
+        self.a3 = a3
+
+    @classmethod
+    def my_class_method(cls):
+        pass
+
+    def my_instance_method(self):
+        pass
+
+
+
